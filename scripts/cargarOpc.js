541aa3219501ba85e7acc93c0fc0532ca17f09df
Chard33 <jchar33@hotmail.com>
2024-10-15 00:12:14 -0600

    Permitiendo conexion

--- scripts/cargarOpc.js
+++ scripts/cargarOpc.js
@@ -3,7 +3,7 @@ async function cargaOpc() {
 
     try {
 
-        const consulta = await fetch("http://localhost:3000/imagenes")
+        const consulta = await fetch("https://api-alura-geek-n7j2z9506-char33s-projects.vercel.app/imagenes")
 
         const lista = document.querySelector("[data-img]")
 
